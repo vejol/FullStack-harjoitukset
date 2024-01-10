de0e0fb43ca2dd0d437f8055d7d9b698f5461286
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2024-01-10 13:42:41 +0200

    Create ./models/blog.js

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -4,15 +4,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const {PORT, MONGODB_URI} = require('./utils/config')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 mongoose.connect(MONGODB_URI)
 
