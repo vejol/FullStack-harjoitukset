d44f9392f8f6aa150dfb23f585aa545c19616f95
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2024-01-10 14:25:32 +0200

    Add app.js

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,18 +1,6 @@
-const express = require('express')
+const app = require('./app')
+const { PORT } = require('./utils/config')
 const logger = require('./utils/logger')
-
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const { PORT, MONGODB_URI } = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
-
-mongoose.connect(MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
 
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
