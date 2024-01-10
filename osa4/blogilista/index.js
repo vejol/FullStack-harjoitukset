a71486fc98d8ac723d994d0bc3b8cdf21872cb1b
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2024-01-10 14:06:42 +0200

    Create controllers/blogs.js

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -4,30 +4,14 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const { PORT, MONGODB_URI } = require('./utils/config')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
